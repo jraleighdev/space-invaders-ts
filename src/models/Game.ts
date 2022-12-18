77a31b58477e53be569dbc742291e2f316df6a03
jraleighdev <39661900+jraleighdev@users.noreply.github.com>
2022-12-18 00:24:36 -0600

    Refactored aliens in multi array.

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -7,7 +7,7 @@ export class Game {
     public static Margin = 10;
 
     public player: Player;
-    public aliens: Alien[] = [];
+    public aliens: Alien[][] = [];
     public direction: Direction = Direction.Right;
     public speedModifier = 1;
 
@@ -19,12 +19,25 @@ export class Game {
         const numberOfColumns = Math.ceil((Game.GameWidth - Game.Margin * 2) / (Alien.Width + alienMargin)) - 4
         const numberOfRows = 10;
         for (let i = 1; i <= numberOfColumns; i++) {
+            const column: Alien[] = [];
             for (let j = 1; j <= numberOfRows; j++) {
                 const x = Game.Margin + (alienMargin + Alien.Width) * i;
                 const y = (alienMargin + Alien.Height) * j;
-                this.aliens.push(new Alien(x, y, i - 1, j - 1, this.ctx));
+                column.push(new Alien(x, y, i - 1, j - 1, this.ctx));
+            }
+            this.aliens.push(column);
+        }
+    }
+
+    private flatAliens(): Alien[] {
+        const tempArray: Alien[] = [];
+        for (let i = 0; i <= this.aliens.length - 1; i++) {
+            const column = this.aliens[i];
+            for (let j = 0; j <= column.length - 1; j++) {
+                tempArray.push(column[j])
             }
         }
+        return tempArray;
     }
 
     public moveAliens() {
@@ -32,30 +45,34 @@ export class Game {
         let xMin = 0
         let yMax = 0;
         for (let i = 0; i <= this.aliens.length - 1; i++) {
-            const alien = this.aliens[i]; 
-            if (alien.hitByBullet) {
-                console.log(alien);
-                this.aliens.splice(i, 1);
-                continue;
+            const column = this.aliens[i];
+            for (let j = 0; j <= column.length - 1; j++) {
+                const alien = column[j];
+                if (alien.hitByBullet) {
+                    console.log(alien);
+                    column.splice(j, 1);
+                    continue;
+                }
+                if (i == 0) {
+                    xMax = alien.bottomRightPoint.x;
+                    xMin = alien.bottomLeftPoint.x;
+                    yMax = alien.bottomLeftPoint.y;
+                } else if (alien.bottomLeftPoint.x < xMin) {
+                    xMin = alien.bottomLeftPoint.x
+                } else if (alien.bottomRightPoint.x > xMax) {
+                    xMax = alien.bottomRightPoint.x;
+                } else if (alien.bottomLeftPoint.y > yMax) {
+                    yMax = alien.bottomLeftPoint.y
+                }
             }
-            if (i == 0) {
-                xMax = alien.bottomRightPoint.x;
-                xMin = alien.bottomLeftPoint.x;
-                yMax = alien.bottomLeftPoint.y;
-            } else if (alien.bottomLeftPoint.x < xMin) {
-                xMin = alien.bottomLeftPoint.x
-            } else if (alien.bottomRightPoint.x > xMax) {
-                xMax = alien.bottomRightPoint.x;
-            } else if (alien.bottomLeftPoint.y > yMax) {
-                yMax = alien.bottomLeftPoint.y
-            }
+
         }
 
         let directionSwitched = false;
 
         switch (this.direction) {
             case Direction.Right:
-                if (xMax > Game.GameWidth - Game.Margin * 2  ) {
+                if (xMax > Game.GameWidth - Game.Margin * 2) {
                     this.direction = Direction.Left;
                     directionSwitched = true;
                 }
@@ -67,7 +84,7 @@ export class Game {
                 }
         }
 
-        this.aliens.forEach(alien => {
+        this.flatAliens().forEach(alien => {
             let newX = alien.x
             // const newY = alien.y;
             switch (this.direction) {
@@ -81,7 +98,7 @@ export class Game {
                     break;
             }
 
-            const newY = directionSwitched ? alien.y += Alien.Speed * 2 * this.speedModifier : alien.y 
+            const newY = directionSwitched ? alien.y += Alien.Speed * 2 * this.speedModifier : alien.y
 
             alien.move(newX, newY)
         })
@@ -89,40 +106,29 @@ export class Game {
 
     checkBulletCollisions() {
         if (this.player.bullets.length === 0) return;
-        const maxColum = Math.max(...this.aliens.map(x => x.column));
-        const columnAliens: Alien[][] = []
-        for (let i = 0; i <= maxColum; i++) {
-            const matchColum = this.aliens.filter(x => x.column == i);
-            columnAliens.push(matchColum);
+        for (let i = 0; i <= this.aliens.length - 1; i++) {
+            const column = this.aliens[i];
+            if (column.length > 0) {
+                const alien = column[column.length - 1];
+                const bullet = this.player.bullets[0];
+                const checkY = bullet.topCenterPoint.y + 2 < alien.bottomLeftPoint.y;
+                const checkX = bullet.topCenterPoint.x > alien.bottomLeftPoint.x && bullet.topCenterPoint.x < alien.bottomRightPoint.x;
+                bullet.hitAlien = checkX && checkY;
+                if (bullet.hitAlien) {
+                    if (this.speedModifier < 3) this.speedModifier += .03;
+                    alien.hitByBullet = true;
+                    break;
+                }
+            }
         }
-        const aliensInBottomRow: Alien[] = [];
-        for (let i = 0; i <= columnAliens.length - 1; i++) {
-            const column = columnAliens[i];
-            const maxRow = Math.max(...column.map(x => x.row));
-            const alien = column.find(x => x.row === maxRow);
-            if (alien) aliensInBottomRow.push(alien);
-        }
-        console.log(aliensInBottomRow);
-        for(let i = 0; i <= aliensInBottomRow.length - 1; i++) {
-            const alien = aliensInBottomRow[i];
-            const bullet = this.player.bullets[0];
-            const checkY = bullet.topCenterPoint.y + 2 < alien.bottomLeftPoint.y;
-            const checkX = bullet.topCenterPoint.x > alien.bottomLeftPoint.x && bullet.topCenterPoint.x < alien.bottomRightPoint.x; 
-            bullet.hitAlien = checkX && checkY;
-            if (bullet.hitAlien) {
-                if (this.speedModifier < 3) this.speedModifier += .03;
-                alien.hitByBullet = true;
-                break;
-            } 
-        }
-    }  
+    }
 
     public draw() {
         this.ctx.fillStyle = 'black';
         this.ctx.fillRect(0, 0, Game.GameHeight, Game.GameWidth);
         this.ctx.strokeStyle = 'white'
         this.ctx.strokeRect(0, 0, Game.GameHeight, Game.GameWidth);
-        this.aliens.forEach(x => x.draw());
+        this.flatAliens().forEach(x => x.draw());
         this.checkBulletCollisions();
         this.moveAliens();
         this.player.draw();
